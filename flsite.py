cde7449bc2db7859bac47bdb15c434327d7599f3
Vadim <vad1mk4@bk.ru>
2024-03-27 13:55:46 +0300

    Тест функции url_for и переменные URL адреса

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -9,13 +9,26 @@ menu = ["Главная", "Первая страница", "Поддержка"]
 @app.route('/index')
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
+    print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/profile/<username>')
+def profile(username):
+    return f'Пользователь: {username}'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username="descr1pt"))
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
