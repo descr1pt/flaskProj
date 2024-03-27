80c343f06e8784c1b633586969c9f1b3ceedc38a
Vadim <vad1mk4@bk.ru>
2024-03-27 12:05:28 +0300

    Добавление шаблонов страниц сайта

--- flsite.py
+++ flsite.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+menu = ["Главная", "Первая страница", "Поддержка"]
 
 
 @app.route('/index')
 @app.route('/')
 def index():
-    return "index"
+    return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
-    return "<h1>О сайте</h1>"
+    return render_template('about.html', title='О сайте', menu=menu)
 
 
 if __name__ == "__main__":
