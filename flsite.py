c21d47dc8a64b99d171301e346b817765ca07122
Vadim <vad1mk4@bk.ru>
2024-04-06 23:29:56 +0300

    Добавил Декоратор errorhandler, функции redirect и abort

--- flsite.py
+++ flsite.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fgfgrgrgfrgrg34gfgfgf6676'
 
 menu = [
+    {'name': 'Главная', 'url': '/'},
+    {'name': 'О сайте', 'url': 'about'},
     {'name': 'Установка', 'url': 'install-flask'},
     {'name': 'Первое приложение', 'url': 'first-app'},
-    {'name': 'Поддержка', 'url': 'contact'}
+    {'name': 'Поддержка', 'url': 'contact'},
+    {'name': 'Авторизоваться', 'url': 'login'},
 ]
 
 
@@ -25,6 +28,9 @@ def about():
 
 @app.route('/profile/<username>')
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
     return f'Пользователь: {username}'
 
 
@@ -36,6 +42,24 @@ def contact():
         else:
             flash('Ошибка отправки', category='error')
     return render_template('contact.html', title='Поддержка', menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'descr1pt' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] != 'descr1pt' and request.form['psw'] != "123":
+        flash('Неверный логин или пароль')
+
+    return render_template('login.html', title='Авторизация', menu=menu)
 
 
 # with app.test_request_context():
